8bfcd664fac2096998d36926caa9364a177c9c90
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2016-07-04 11:08:52 +0300

    [ImageLoader] Documentation

--- ImageLoader.java
+++ ImageLoader.java
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * <p/>
+ * <p>
  * Copyright (c) 2016 Yuriy Budiyev [yuriy.budiyev@yandex.ru]
- * <p/>
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p/>
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * <p/>
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -1480,7 +1480,7 @@ public class ImageLoader<T> {
     }
 
     /**
-     * Bitmap loader from concrete source data type
+     * Bitmap loader from source data
      *
      * @param <T> Source data type
      */
@@ -1498,9 +1498,22 @@ public class ImageLoader<T> {
      * Callback of image loading
      */
     public interface Callback {
+        /**
+         * Called when image is loaded and ready to be displayed
+         *
+         * @param image            Image
+         * @param fromMemoryCache  Image loaded from memory cache
+         * @param fromStorageCache Image loaded from storage cache
+         */
         void onImageLoaded(@Nullable Bitmap image, boolean fromMemoryCache,
                 boolean fromStorageCache);
 
+        /**
+         * Called when image displayed; if fade enabled, this method will be called
+         * when fade will done
+         *
+         * @param imageView Image view
+         */
         void onImageDisplayed(@NonNull ImageView imageView);
     }
 }
