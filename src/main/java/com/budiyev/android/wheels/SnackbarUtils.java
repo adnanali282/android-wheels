ca55c5c3132ab958ce26745f42aff7d6df119c81
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2016-07-21 15:53:51 +0300

    Reworked SnackbarUtils due to compatibility issues

--- src/main/java/com/budiyev/android/wheels/SnackbarUtils.java
+++ src/main/java/com/budiyev/android/wheels/SnackbarUtils.java
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * <p>
+ * <p/>
  * Copyright (c) 2016 Yuriy Budiyev [yuriy.budiyev@yandex.ru]
- * <p>
+ * <p/>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p>
+ * <p/>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * <p>
+ * <p/>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -44,25 +44,7 @@ public final class SnackbarUtils {
     }
 
     /**
-     * Wrap {@link SnackbarAction} into {@link View.OnClickListener}
-     *
-     * @param action   Action
-     * @param snackbar Snackbar
-     * @return OnClickListener
-     */
-    @NonNull
-    private static View.OnClickListener wrapAction(@NonNull final SnackbarAction action,
-            @NonNull final Snackbar snackbar) {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                action.onClick(v, snackbar);
-            }
-        };
-    }
-
-    /**
-     * Find suitable view for {@link Snackbar}
+     * Find suitable view for Snackbar
      *
      * @param view View
      * @return Best suitable view
@@ -96,7 +78,7 @@ public final class SnackbarUtils {
     }
 
     /**
-     * Make {@link Snackbar} for specified {@link View}
+     * Make Snackbar for specified {@link View}
      *
      * @param view     View
      * @param text     Message text
@@ -110,7 +92,7 @@ public final class SnackbarUtils {
     }
 
     /**
-     * Make {@link Snackbar} for specified {@link View}
+     * Make Snackbar for specified {@link View}
      *
      * @param view     View
      * @param textId   Message text resource identifier
@@ -124,7 +106,7 @@ public final class SnackbarUtils {
     }
 
     /**
-     * Make {@link Snackbar} for specified {@link Window}
+     * Make Snackbar for specified {@link Window}
      *
      * @param window   Window
      * @param text     Message text
@@ -146,7 +128,7 @@ public final class SnackbarUtils {
     }
 
     /**
-     * Make {@link Snackbar} for specified {@link Window}
+     * Make Snackbar for specified {@link Window}
      *
      * @param window   Window
      * @param textId   Message text resource identifier
@@ -168,7 +150,7 @@ public final class SnackbarUtils {
     }
 
     /**
-     * Make {@link Snackbar} for specified {@link Activity}
+     * Make Snackbar for specified {@link Activity}
      *
      * @param activity Activity
      * @param text     Message text
@@ -186,7 +168,7 @@ public final class SnackbarUtils {
     }
 
     /**
-     * Make {@link Snackbar} for specified {@link Activity}
+     * Make Snackbar for specified {@link Activity}
      *
      * @param activity Activity
      * @param textId   Message text resource identifier
@@ -204,7 +186,7 @@ public final class SnackbarUtils {
     }
 
     /**
-     * Make {@link Snackbar} for specified {@link Fragment}
+     * Make Snackbar for specified {@link Fragment}
      *
      * @param fragment Fragment
      * @param text     Message text
@@ -222,7 +204,7 @@ public final class SnackbarUtils {
     }
 
     /**
-     * Make {@link Snackbar} for specified {@link Fragment}
+     * Make Snackbar for specified {@link Fragment}
      *
      * @param fragment Fragment
      * @param textId   Message text resource identifier
