bed6490c81545e3ee7d013e57d54f40b8191dec8
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2016-07-21 16:39:46 +0300

    Renamed Size class to SizeCompat

--- src/main/java/com/budiyev/android/wheels/CommonUtils.java
+++ src/main/java/com/budiyev/android/wheels/CommonUtils.java
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * <p/>
+ * <p>
  * Copyright (c) 2016 Yuriy Budiyev [yuriy.budiyev@yandex.ru]
- * <p/>
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p/>
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * <p/>
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -70,7 +70,7 @@ public final class CommonUtils {
      * @return Real size if view already displayed, measured size otherwise
      */
     @NonNull
-    public static Size getViewSize(@NonNull View view) {
+    public static SizeCompat getViewSize(@NonNull View view) {
         int width = view.getWidth();
         int height = view.getHeight();
         if (width == 0 || height == 0) {
@@ -83,7 +83,7 @@ public final class CommonUtils {
             width = view.getMeasuredWidth();
             height = view.getMeasuredHeight();
         }
-        return new Size(width, height);
+        return new SizeCompat(width, height);
     }
 
     /**
--- src/main/java/com/budiyev/android/wheels/SizeCompat.java
+++ src/main/java/com/budiyev/android/wheels/SizeCompat.java
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * <p/>
+ * <p>
  * Copyright (c) 2016 Yuriy Budiyev [yuriy.budiyev@yandex.ru]
- * <p/>
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p/>
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * <p/>
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,21 +26,22 @@ package com.budiyev.android.wheels;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.util.Size;
 
-public final class Size {
+public final class SizeCompat {
     private final int mWidth;
     private final int mHeight;
 
-    public Size(int width, int height) {
+    public SizeCompat(int width, int height) {
         mWidth = width;
         mHeight = height;
     }
 
     /**
-     * Copy values from {@link android.util.Size}
+     * Copy values from {@link Size}
      */
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public Size(@NonNull android.util.Size size) {
+    public SizeCompat(@NonNull Size size) {
         mWidth = size.getWidth();
         mHeight = size.getHeight();
     }
@@ -54,20 +55,20 @@ public final class Size {
     }
 
     /**
-     * Convert to {@link android.util.Size}
+     * Convert to {@link Size}
      */
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @NonNull
-    public android.util.Size toAndroidSize() {
-        return new android.util.Size(mWidth, mHeight);
+    public Size toSize() {
+        return new Size(mWidth, mHeight);
     }
 
     @Override
     public boolean equals(Object obj) {
         if (obj == this) {
             return true;
-        } else if (obj instanceof Size) {
-            Size size = (Size) obj;
+        } else if (obj instanceof SizeCompat) {
+            SizeCompat size = (SizeCompat) obj;
             return mWidth == size.getWidth() && mHeight == size.getHeight();
         } else {
             return false;
